9a12fc0566d1223c08fd09eeec41d83e9a7a989b
Descultu Cristian <56123978+HyperSmoit@users.noreply.github.com>
2019-11-26 02:59:12 +0200

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -4,25 +4,45 @@ public class GameEngine {
 
     public void fight(ArrayList<Player> players) {
         for (Player player : players) {
+            player.fighting(players);
+        }
 
+        for (Player player : players) {
+            player.setFight(false);
         }
     }
 
     public void startGame(GameInput gameInput, ArrayList<Player> players, Map map) {
-        int i = 0;
-        for (ArrayList<Character> arr : gameInput.getDirections()) {
+        for (int i = 0; i < gameInput.getNrOfRounds(); ++i) {
+            System.out.println("Inceputul rundei  " + i);
             for (Player player : players) {
-                player.move(arr.get(players.indexOf(player)), map.getPlayGround().get(0).size(), map);
+                System.out.println(player.getHp());
+              //  System.out.println("POZITIA: " + player.getCoordinateX() + " " + player.getCoordinateY());
+                player.dotEffect();
             }
-            System.out.println("RUNDA: " + ++i);
+            fight(players);
+            System.out.println("FINALUL RUNDEI:  " + i);
+            for (Player player : players) {
+                System.out.println(player.getHp());
+                player.move(player.getDirections().get(i), map.getPlayGround().get(0).size(), map);
+              //  System.out.println("POZITIA DUPA MUTARE: " + player.getCoordinateX() + " " + player.getCoordinateY());
+                if (player instanceof Rogue) {
 
-            for (Player player : players) {
-                player.fighting(players);
-            }
-            for (Player player : players) {
-                player.setFight(false);
-            }
+                } else if (player instanceof Wizard) {
 
+
+                } else if (player instanceof Pyromancer) {
+
+                } else if (player instanceof  Knight) {
+
+                }
+            }
+        }
+        fight(players);
+        System.out.println("THE END");
+        for (Player player : players) {
+            player.levelUp();
+            System.out.println(player.getType() + " " + player.getLevel() + " " + player.getXp() + " " + player.getHp());
         }
     }
 
