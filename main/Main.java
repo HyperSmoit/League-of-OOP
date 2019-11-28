04467a6291826d806f5d79fe8d36b78a17104ff4
Descultu Cristian <56123978+HyperSmoit@users.noreply.github.com>
2019-11-28 09:31:17 +0200

    Update and rename Main.java to main/Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,16 +1,18 @@
+package main;
+import heroes.Player;
+import map.Map;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
-//package com.tema2.main;
-public class Main {
+abstract class Main {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) throws IOException {
         GameInputLoader gameInputLoader = new GameInputLoader(args[0], args[1]);
         GameInput gameInput = gameInputLoader.load();
         GameEngine gameEngine = new GameEngine();
         ArrayList<Player> players = gameInput.getPlayers();
         Map map = new Map(gameInput.getMap());
-        gameEngine.startGame(gameInput, players, map);
-
-        // write your code here
+        gameEngine.startGame(gameInput, players, map, gameInputLoader.getFs());
     }
 }
